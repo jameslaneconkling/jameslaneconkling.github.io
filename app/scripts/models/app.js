077c2de21ccb4fb53e4048ae026d8f25e5269159
James Conkling <james.lane.conkling@gmail.com>
2017-07-29 17:49:42 -0400

    replace twitter w/ linkedin

--- app/scripts/models/app.js
+++ app/scripts/models/app.js
@@ -3,7 +3,7 @@ var Backbone = require('backbone');
 module.exports = Backbone.Model.extend({
 
   initialize: function() {
-    this.set('twitter', 'jamesconkling');
+    this.set('linkedin', 'jameslaneconkling');
     this.set('github', 'jameslaneconkling');
     this.set('email', 'james.lane.conkling@gmail.com');
     this.set('collection', null);
